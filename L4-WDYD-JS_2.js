d2b44bd5e7f15e31e674869f1f22cb855bbadc77
humbatovqara <qarahumbtov@gmail.com>
2021-02-08 23:04:15 +0400

    Day 2

---
+++ L4-WDYD-JS_2.js
@@ -0,0 +1,12 @@
+function openLight() {
+    document.getElementById("Image").src="../../image/open-light.jpg";
+}
+
+function closeLight() {
+    document.getElementById("Image").src="../../image/close-light.png";
+}
+
+function changeCSS() {
+    document.getElementById("result").style.color = "blue";
+    document.getElementById("result").style.fontSize = "30px";
+}
