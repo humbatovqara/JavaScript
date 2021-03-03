9140ffe93d5e8b78ba5808b8ea0abbc775123cae
humbatovqara <qarahumbtov@gmail.com>
2021-03-03 22:00:41 +0400

    Day 3

---
+++ L6-Print-method.js
@@ -0,0 +1,12 @@
+// Üsul 1 / HTML elementinə innerHTML yazaraq
+document.getElementById("result").innerHTML = "JS innerHTML";
+
+// Üsul 2 / document.write
+document.write("JS document.write()");
+
+// Üsul 3 / window.alert
+window.alert("Alert message");
+alert("Alert message 2");
+
+// Üsul 4 / console.log
+console.log("F12 click");
